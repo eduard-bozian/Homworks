35d7e4ddf87f31e9cb20863d7fa784558a409a88
Эдуард <eduard.bzian@gmail.com>
2024-08-18 09:20:36 +0300

    module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -1,9 +1,9 @@
 def add_everything_up(a, b):
     try:
-        if a != b:
-         return str(a) + str(b)
+        return a + b
     except TypeError:
-        print(f'Не верный тип данных!')
+        return str(a) + str(b)
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
+
